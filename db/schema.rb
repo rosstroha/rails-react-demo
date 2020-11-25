ce33a05cb89ca51b5416ff59ce140dd2252828fb
Ross Troha <rosstroha@gmail.com>
2020-11-25 17:43:40 -0500

    schema.rb update after migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_24_215917) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.text "poster"
     t.text "title"
