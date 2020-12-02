e432d996153d2c44c677898600d3ce1f7c9b82d0
Ross Troha <rosstroha@gmail.com>
2020-12-01 19:50:30 -0500

    render more than one movie in the client

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from 'react'
 import { Col, Row } from 'react-materialize'
 import { MoviePoster } from './MoviePoster.js'
 import { MovieSearch } from './MovieSearch.js'
+import { find } from 'lodash'
 
 export function App (props) {
   const [imdbId, setImdbId] = useState('tt4158110')
   const [title, setTitle] = useState('')
   const [plot, setPlot] = useState('')
   const [poster, setPoster] = useState('')
+  const [allMovies, setAllMovies] = useState([])
 
   const handleIdChange = (event) => {
     setImdbId(event.target.value)
   }
 
   const fetchMovie = async () => {
-    let response = await fetch(`/movie/${imdbId}`)
-    let {title, poster, plot} = await response.json()
-    setTitle(title)
-    setPlot(plot)
-    setPoster(poster)
+    if (!find(allMovies, { imdb_id: imdbId })) {
+      let response = await fetch(`/movie/${imdbId}`)
+      let movie = await response.json()
+
+      setAllMovies(allMovies => [...allMovies, movie])
+    }
   }
 
   const handleSubmit = (event) => {
@@ -26,13 +29,22 @@ export function App (props) {
     fetchMovie()
   }
 
-  const posterComponent = () => {
-    if (!title) { return null }
-    return <MoviePoster poster={poster} title={title} plot={plot} />
+  const posterComponent = ({poster, title, plot}) => {
+    return (
+      <Col>
+        <MoviePoster poster={poster} title={title} plot={plot} />
+      </Col>
+    )
+  }
+
+  const fetchAllMovies = async () => {
+    let response = await fetch('/movies.json')
+    let movies = await response.json()
+    setAllMovies(movies)
   }
 
   useEffect(() => {
-    fetchMovie()
+    fetchAllMovies()
   }, [imdbId])
 
   return (
@@ -43,9 +55,7 @@ export function App (props) {
         </Col>
       </Row>
       <Row>
-        <Col>
-          {posterComponent()}
-        </Col>
+        {allMovies.map(movie => posterComponent(movie))}
       </Row>
     </React.Fragment>
   )
